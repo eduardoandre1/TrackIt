7284d8ce7cb89e8137fa2b17998a0260e89862df
Eduardo André <eduardoandre418@gmail.com>
2023-06-04 23:13:52 -0300

    alteração de cor no status

--- src/pages/Today/Today-style.js
+++ src/pages/Today/Today-style.js
@@ -1,32 +1,34 @@
 import styled from "styled-components";
 
 const Today_habit = styled.div`
+all: unset;
 width: 340px;
 height: 94px;
 background: #FFFFFF;
 border-radius: 5px;
 position: relative;
 margin: 5px;
-flex-wrap: wrap;
+display: flex;
+flex-direction: column;
+align-items: first baseline;
 
 h1{
+    width: 100%;
     font-family: 'Lexend Deca';
     font-style: normal;
     font-weight: 400;
     font-size: 19.976px;
     line-height: 25px;
     color: #666666;
-    overflow: hidden;
     margin-top: 5px;
-    text-align: center;
 }
 h2{
     font-family: 'Lexend Deca';
     font-style: normal;
     font-weight: 400;
-    font-size: 12.976px;
+    font-size: 12px;
     line-height: 16px;
-    color: #666666;
+    color: #666666;;
 }
 `
 const Greenbutton = styled.div`
@@ -41,10 +43,25 @@ const Greenbutton = styled.div`
     top: 12px;
     right: 5px;
 `
-const Green =styled.h2`
- color:#8FC549;`
+const Greentext =styled.div`
+    all: unset;
+    font-family: 'Lexend Deca';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 16px;
+    color: #8FC549;
+`
+const Blacktext =styled.h2`
+    all: unset;
+    font-family: 'Lexend Deca';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12.976px;
+    line-height: 16px;
+    color: #2C2C2C;`
 
 const Graybutton = styled(Greenbutton)`
 background-color: #EBEBEB;`
 
-export { Today_habit ,Greenbutton , Graybutton ,Green}
+export { Blacktext, Today_habit ,Greenbutton , Graybutton ,Greentext}
